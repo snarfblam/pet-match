4fc1dfd40c45adc56430b043b82638ff2716ed16
Tom H <snarfblam@gmail.com>
2018-03-30 22:30:49 -0400

    Created skeleton structure for views.

--- routes/httpRoutes.js
+++ routes/httpRoutes.js
@@ -5,5 +5,29 @@ var router = express.Router();
 router.get('/', (req, res) => {
     res.render('index');
 });
+router.get('/rescues', (req, res) => {
+    res.render('rescues');
+});
+router.get('/rescueDetails', (req, res) => {
+    res.render('rescueDetails');
+});
+router.get('/events', (req, res) => {
+    res.render('events');
+});
+router.get('/eventDetails', (req, res) => {
+    res.render('eventDetails');
+});
+router.get('/addEvent', (req, res) => {
+    res.render('addEvent');
+});
+router.get('/profile', (req, res) => {
+    res.render('profile');
+});
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+router.get('/register', (req, res) => {
+    res.render('register');
+});
 
 module.exports = router;
