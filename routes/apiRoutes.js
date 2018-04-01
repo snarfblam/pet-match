44ad579eee6b5585a1872079e17b13994d48601d
Tom H <snarfblam@gmail.com>
2018-04-01 18:47:58 -0400

    Added post/put/delete routes for event model

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+var models = require('../models');
+
 // The routes specified in this file will be mounted at '/api'.
 // For example, app.post('createUser') will be accessible at '/api/createUser'
 
@@ -6,26 +8,106 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('login', (req, res) => {
+router.post('/login', (req, res) => {
     res.status('501').end();
 });
-router.post('createAccount', (req, res) => {
+router.post('/createAccount', (req, res) => {
     res.status('501').end();
+});
+// Creates a new event listing
+// Expects: {userId, date, address, city, state, zip, description, RSVP}
+// Returns: 
+//  {
+//      status: "error" | "inserted",
+//      error?: string,
+//      value?: (inserted record)
+//  }
+// Todo: some kind of user or user token validation
+router.post('/editInfo', (req, res) => {
+    // models.user.create({firstname: "tom",lastname:"my", username: "tommy", about: "sup", email:"a@b.com", password: "dog", last_login:Date(), status: 'active'}).then(r=>console.log(r)).catch(e=>console.log(e))
+    models.events.create({
+        userId: req.body.userId,
+        location: req.body.location,
+        description: req.body.description,
+        RSVP: req.body.RSVP,
+        date: req.body.date,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+    }).then(result => {
+        console.log(result);
+        res.json({
+            status: 'inserted',
+            value: result,
+        });
+    }).catch(e => {
+        res.json({
+            status: 'error',
+            error: e.message || e.msg || e || "unknown error",
+        });
+    });
+    // res.status('501').end();
 });
 
-router.post('editInfo', (req, res) => {
-    // create event
-    res.status('501').end();
-});
-router.put('editInfo', (req, res) => {
+// Modifies an existing even listing
+// Expects: {date?, address?, city?, state?, zip?, description?, RSVP?}
+// Returns: 
+//  {
+//      status: "error" | "updated",
+//      error?: string,
+//      affectedRows?: number
+//  }
+// Todo: some kind of user or user token validation
+router.put('/editInfo/:id', (req, res) => {
     // update event
-    res.status('501').end();
+    models.events.update({
+        location: req.body.location,
+        description: req.body.description,
+        RSVP: req.body.RSVP,
+        date: req.body.date,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+    }, {
+        where: {
+            id: req.params.id,    
+    }}).then(result => {
+        console.log(result);
+        res.json({
+            status: 'updated',
+            affectedRows: result[0],
+        });
+    }).catch(e => {
+        res.json({
+            status: 'error',
+            error: e.message || e.msg || e || "unknown error",
+        });
+    });
 });
-router.delete('editInfo', (req, res) => {
-    // remove event
-    res.status('501').end();
+// Removes and existing event entry by ID
+// Returns: 
+//  {
+//      status: "error" | "removed",
+//      error?: string,
+//  }
+// Todo: some kind of user or user token validation
+router.delete('/editInfo/:id', (req, res) => {
+    models.events.destroy({
+        where: { id: req.params.id }
+    }).then(result => {
+        console.log(result);
+        if (result == 0) {
+            res.json({ status: 'error', error: 'no match found' });
+        } else {
+            res.json({ status: 'removed' });
+        }
+    }).catch(e => {
+        res.json({ status: 'error', error: 'no match found' });
+    });
 });
-router.post('profile', (req, res) => {
+router.post('/profile', (req, res) => {
     res.status('501').end();
 });
 
